dd519237a48197d7f9a9f6d6a7244ce49b424727
Pavel Druzyak <pavel.druzyak@gmail.com>
2011-12-15 05:34:19 +0200

    Feature create new template

--- features/step_definitions/template_steps.rb
+++ features/step_definitions/template_steps.rb
@@ -20,6 +20,14 @@ When /^I look at the list of templates$/ do
   visit '/templates'
 end
 
+When /^I create new template$/ do
+  visit '/templates'
+  within("#new-template") do
+    fill_in "title", with: 'Banana Pie'
+    click_button "Create Template"
+  end
+end
+
 ### THEN ###
 Then /^I should see all my templates$/ do
   valid_templates.each do |title|
@@ -31,4 +39,9 @@ Then /^I should not see his templates$/ do
   valid_templates.each do |title|
     find("#templates").should_not have_content("#{title}!!!")
   end
+end
+
+Then /^I should see this template$/ do
+  find("#templates").should have_content("Banana Pie")
+  Template.should exist(conditions: { title: "Banana Pie" })
 end
--- features/templates/template_list.feature
+++ features/templates/template_list.feature
@@ -3,16 +3,17 @@ Feature: Template lists
   I want to to be able to all my templats
   So I can manage them
 
-    @javascript
-    Scenario: View my templates
+    Background:
       Given I am logged in
+
+    @template @javascript
+    Scenario: View my templates
       Given I have few templates
       When I look at the list of templates
       Then I should see all my templates
 
-    @javascript
+    @template @javascript
     Scenario: I don't see templates of other users
-      Given I am logged in
       Given another user exists
         And he have few templates too
       When I look at the list of templates
