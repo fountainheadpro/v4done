3afa57f38b6af846a4202a05c251264037e7fa01
Pavel Druzyak <pavel.druzyak@gmail.com>
2012-02-22 15:02:03 +0200

    Add styles for flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def css_class_for_flash(name)
+    case name
+    when :alert
+      'alert-error'
+    when :notice
+      'alert-success'
+    else
+      "alert-info"
+    end
+  end
 end
