d14b1a975211491aa416a888bdabf21c2237b2e3
Pavel Druzyak <pavel.druzyak@gmail.com>
2012-03-01 18:16:57 +0200

    Add more specs and implementation

--- app/models/concerns/mongoid_embedded_tree.rb
+++ app/models/concerns/mongoid_embedded_tree.rb
@@ -5,27 +5,32 @@ module Mongoid::EmbeddedTree
     field :parent_id
     field :parent_ids, :type => Array, :default => []
     index :parent_ids
+
+    scope :roots, where(parent_id: nil)
+
+    set_callback :save, :before, :update_path
   end
 
   module InstanceMethods
     def parent
-      nil
+      _parent.nodes.find(parent_id) unless parent_id.nil?
     end
 
     def root?
-      true
-    end
-
-    def root
-      nil
+      parent_id.nil?
     end
 
     def leaf?
-      true
+      children.empty?
     end
 
     def children
-      []
+      _parent.nodes.where(parent_id: id)
+      # [self.class.to_s.underscore.pluralize.to_sym]
+    end
+
+    def update_path
+      self.parent_ids = parent.parent_ids + [self.parent_id] unless self.parent_id.nil?
     end
   end
 end
--- spec/models/concerns/mongoid_embedded_tree_spec.rb
+++ spec/models/concerns/mongoid_embedded_tree_spec.rb
@@ -4,8 +4,10 @@ describe Mongoid::EmbeddedTree do
   shared_examples_for "a root node" do
     its (:parent_ids) { should be_empty }
     its (:parent) { should be_nil }
-    its (:root) { should be_nil }
     it { should be_root }
+    it "presents in roots" do
+      container.nodes.roots.should include(subject)
+    end
   end
 
   shared_examples_for "a leaf node" do
@@ -13,13 +15,75 @@ describe Mongoid::EmbeddedTree do
     it { should be_a_leaf }
   end
 
+  shared_examples_for "a parent node" do
+    its (:children) { should_not eq([]) }
+    it { should_not be_a_leaf }
+  end
+
+  shared_examples_for "a child node" do
+    its (:parent_ids) { should_not be_empty }
+    its (:parent_ids) { should include(parent.id) }
+    its (:parent) { should eq(parent) }
+    it { should_not be_root }
+    it "not presents in roots" do
+      container.nodes.roots.should_not include(subject)
+    end
+  end
+
   describe "A node" do
-    let(:container) { Container.create :name => "A Container"}
-    let(:node) { container.nodes.create :name => "A Node"}
-    subject {node}
+    let(:container) { Container.create name: "A Container"}
+    let(:node) { Node.new name: "A Node"}
+    subject { node }
+
+    before(:each) do
+      container.nodes << node
+      node.reload
+    end
 
     it { should be_valid }
     it_should_behave_like "a leaf node"
     it_should_behave_like "a root node"
+
+    it "should exist 1 nodes" do
+      container.nodes.count.should be(1)
+    end
+
+    context "when adding a child" do
+      let(:parent) { node }
+      let(:child) { Node.new(name: "A Child", parent_id: parent.id)}
+
+      before(:each) do
+        container.nodes << child
+        child.reload
+      end
+
+      it_should_behave_like "a parent node"
+      it_should_behave_like "a root node"
+
+      it "should change the children count" do
+        expect{
+          container.nodes << Node.new(name: "another child", parent_id: parent.id)
+        }.to change{node.children.count}.by(1)
+      end
+
+      it "should exist 2 nodes" do
+        container.nodes.count.should be(2)
+      end
+
+      it "should have 1 child" do
+        node.children.count.should be(1)
+      end
+
+      it "should be able to access the child" do
+        node.children.first.should eq(child)
+      end
+
+      context "the child" do
+        subject { child }
+        it_should_behave_like "a leaf node"
+        it_should_behave_like "a child node"
+      end
+
+    end
   end
 end
