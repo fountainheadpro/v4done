7259150e102fea45573cb6ead46dccf01cbaee22
Pavel Druzyak <pavel.druzyak@gmail.com>
2012-03-20 03:27:27 +0200

    Adds factory for the project and first step

---
+++ features/step_definitions/action_steps.rb
@@ -0,0 +1,4 @@
+### GIVEN ###
+Given /^I have project with few actions and subactions$/ do
+  @project = Factory.create(:project_with_actions)
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -34,4 +34,27 @@ FactoryGirl.define do
     creator
     template { Factory.create(:template_with_subitems, creator: creator) }
   end
+
+  factory :project do
+    creator
+    sequence(:title) { |n| "Project #{n}" }
+
+    factory :project_with_actions do
+      ignore do
+        actions_count 2
+        subactions_count 3
+      end
+
+      after_create do |project, evaluator|
+        action = nil
+        evaluator.actions_count.times do |i|
+          action = project.actions.create title: "Action #{i}", previous_id: action.try(:id)
+          subaction = nil
+          evaluator.subactions_count.times do |j|
+            subaction = project.actions.create title: "Subaction #{j}", parent_id: action.id, previous_id: subaction.try(:id)
+          end
+        end
+      end
+    end
+  end
 end
