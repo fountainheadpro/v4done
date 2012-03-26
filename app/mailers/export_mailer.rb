c928f9f72ff3ea2af02431482177c518cb1dd7fa
Pavel Druzyak <pavel.druzyak@gmail.com>
2012-03-26 20:08:52 +0300

    Revert "Mailgun recommended to from this address"
    This reverts commit c1b293cb7ca4404f0839983714d8186243e21e09.

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,6 +1,11 @@
 class ExportMailer < ActionMailer::Base
-  default from: "subscription@actions-im.mailgun.org"
+  default from: "subscription@actions.im"
 
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.export_mailer.actions.subject
+  #
   def actions(project)
     @project = project
     mail to: project.owner[:email] || project.owner['email'], subject: project.title
