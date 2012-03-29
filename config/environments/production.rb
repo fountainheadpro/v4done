c3199caef1b0f1c6f259490bccb06c99ad6aca79
Sergey Zelvenskiy <sergey@actions.im>
2012-03-29 00:58:24 -0700

    config.assets.initialize_on_precompile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Actions::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = true              ``
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
@@ -44,6 +44,7 @@ Actions::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( mobile.css project_tools_app.js maven_tools_app.js )
+  config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
