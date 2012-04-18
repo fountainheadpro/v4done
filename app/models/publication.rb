5446f7add14bd4d26af279a9ae0919d578430d7d
Sergey Zelvenskiy <sergey@actions.im>
2012-04-18 00:37:46 -0700

    newsfeed

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -6,7 +6,7 @@ class Publication
   embeds_one :template
 
   def self.newest()
-    where(:created_at => {'$gte' => DateTime.now-300.days,'$lt' => DateTime.now})
+    where(:created_at => {'$gte' => DateTime.now-300.days,'$lt' => DateTime.now}).asc(:updated_at).entries
   end
 
 end
