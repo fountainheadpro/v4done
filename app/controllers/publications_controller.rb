6ce44004ae73cfac89a4880518facfb71b4989e1
Pavel Druzyak <pavel.druzyak@gmail.com>
2012-04-30 19:48:40 +0300

    Refactoring

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -11,10 +11,11 @@ class PublicationsController < ApplicationController
   # POST /templates/1/publications.html
   def create
     @publication = @template.publication
+    template_attributes = @template.attributes.merge("items" => @template.active_items.entries)
     if @publication.blank?
-      @publication = current_user.publications.create template: { title: @template.title, description: @template.description, created_at: @template.created_at, updated_at: @template.updated_at, items: @template.active_items }
+      @publication = current_user.publications.create template: template_attributes
     else
-      @publication.update_attributes template: { title: @template.title, description: @template.description, updated_at: @template.updated_at, items: @template.active_items }
+      @publication.update_attributes template: template_attributes
       #Publication.collection.find_and_modify(:query => { "_id" => @publication.id }, :update=>@publication.attributes.merge({:template => @template.attributes}) )
     end
     respond_with(@publication)
